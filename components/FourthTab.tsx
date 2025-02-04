0397b873b7e68caa1c2270192461d3de95359821
Olaniyi John <olaniyijohnayomide@gmail.com>
2025-02-04 08:01:40 +0100

    Add new content and improve layout in Hero and FourthTab components

--- components/FourthTab.tsx
+++ components/FourthTab.tsx
@@ -5,6 +5,7 @@ import Team1 from "../assets/png/team1.png";
 import LeadershiptImage from "../assets/png/leadership.png";
 import Natasha from "../assets/png/Natasha.png";
 import Shaun from "../assets/png/Shaun.png";
+import davidTrack from "../assets/png/davidTrack.png";
 
 function FourthTab() {
   return (
@@ -63,12 +64,11 @@ function FourthTab() {
               >
                 A seasoned entrepreneur with over three decades of diverse
                 experience spanning various industries. Beginning his journey
-                with certifications in computer programming, including A+ and
-                Net+, Shaun quickly established himself as a dynamic force in
-                the tield. Transitioning Beam estly into the world often and
-                television, he spent aver a decade as an executive producer and
-                director of chatography, garnering numerous intentions and
-                neconal eccoleoes alone the way.
+                with certifications in computer programming, including A+, Net+
+                and C+ Programing. Shaun quickly established himself as a
+                dynamic force in the field utilizing the latest technologies
+                available while exploring new possibilities within the world of
+                digital capture and process computing.
               </p>
             </div>
           </div>
@@ -79,23 +79,120 @@ function FourthTab() {
             data-aos-duration="1000"
             className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-10"
           >
-            His tenure in manufacturing and production across emerging
-            industries further solidified his reputation as a visionary leader.
-            Shaun's contributions have been recognized with national awards, and
-            his expertise sought after by major world governments, multi-billion
-            dollar corporations, and prestigious universities. Notably, he
-            played a pivotal role in shaping the procedures and regulations for
-            an entire emerging manufacturing and distribution sector. <br /> In
-            2013, Shaun embarked on his current journey into software and
-            cryptocurrency, driven by his passion for artificial intelligence,
-            quantum security, blockchain DeFi, and software development. His
-            dedication and innovation led to the successful launch of his dan
-            software and cryptocurrency ecosystem propelling his company to a
-            remarkable 16x growth. <br /> Despite his numerous achievements,
-            Shaun remains committed to pushing the boundaries of innovation,
-            believing that his best contributions are yet to come. With a
-            relentless pursuit of new challenges and opportunities, Shaun
-            continues to shape the future of technology and business
+            Transitioning seamlessly into the world of Digital Cinematography
+            and television, he pioneered over a decade in this newly emerging
+            field working primarily as an executive producer and director of
+            cinematography. During his career he garnered numerous National and
+            International Film and Television awards for his creativity and
+            knowledge in digital capture and processing.
+          </p>
+
+          <p
+            data-aos="fade-down"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+            className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-10"
+          >
+            His tenure and experience in all stages of market production across
+            several emerging industries including software, digital assets,
+            agriculture, medical and the latest CGMP processes further
+            solidified his reputation as a visionary leader.
+          </p>
+
+          <p
+            data-aos="fade-down"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+            className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-10"
+          >
+            Shaun's contributions and expertise employed him as a highly coveted
+            consultant by several major world governments, multi-billion dollar
+            corporations, and top universities as a critical asset to their
+            endeavors. Notably, he played a pivotal role in shaping the
+            procedures and regulations for an entire emerging manufacturing and
+            distribution sector of many local, national and international
+            industries.
+          </p>
+
+          <p
+            data-aos="fade-down"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+            className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-10"
+          >
+            Despite his numerous achievements, Shaun remains committed to
+            pushing the boundaries of innovation, instinctively knowing that his
+            best contributions have yet to come. With a relentless pursuit of
+            new challenges and opportunities, Shaun continues to shape the
+            future of emerging technologies and business leadership.
+          </p>
+        </div>
+
+        <div className="w-full h-auto md:custom-gradient mt-16 rounded-[32px] md:p-10">
+          <div className="w-full h-auto md:h-[408px] flex flex-col md:flex-row items-center">
+            <Image
+              data-aos="fade-right"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="1000"
+              src={davidTrack}
+              alt="davidTrack"
+              className="w-full md:w-[400px] h-[400px]"
+            />
+            <div className="md:ml-10">
+              <p
+                data-aos="fade-down"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="1000"
+                className="text-[#FFFFFF] text-[36px] font-semibold capitalize text-start my-5 md:my-0"
+              >
+                David Track
+              </p>
+              <p
+                data-aos="fade-up"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="1000"
+                className="text-[#D9D9D9] text-[14px] font-medium uppercase text-start mt-2"
+              >
+                Chief Technology Officer
+              </p>
+
+              <p
+                data-aos="fade-down"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="1000"
+                className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-4"
+              >
+                David Track is a seasoned C-level executive, serial
+                entrepreneur, and consultant with a proven track record of
+                building innovative and successful businesses in the Fintech and
+                Blockchain industries.
+              </p>
+            </div>
+          </div>
+
+          <p
+            data-aos="fade-down"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+            className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-10"
+          >
+            As the former CEO of Crispmind.io, Tectum.io, and Softnote.com, and
+            the founder of numerous startups, David has a unique ability to
+            envision bold, groundbreaking ideas and transform them into
+            impactful ventures. His expertise lies in conceptualizing ambitious
+            projects and executing them with precision, driving growth and
+            innovation in dynamic, fast-paced industries.
+          </p>
+
+          <p
+            data-aos="fade-down"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="1000"
+            className="text-[#FFFFFF] text-[16px] md:text-[18px] font-normal text-start mt-10"
+          >
+            With extensive experience in leadership, strategy, and business
+            development, David continues to shape the future of technology and
+            entrepreneurship.
           </p>
         </div>
       </div>
@@ -111,7 +208,6 @@ function FourthTab() {
         </p>
 
         <div className="w-full h-auto flex flex-col md:flex-row justify-center items-center mt-16">
-
           <div className="w-full md:w-[30%] h-auto">
             <Image
               data-aos="fade-up"
@@ -138,8 +234,6 @@ function FourthTab() {
               COO and co-founder
             </p>
           </div>
-
-         
         </div>
       </div>
     </ContainerLayout>
